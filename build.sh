668596abcb3f5195c51a5b08c7c45cec9127fbe7
Michael Usher <github@usher.au>
2025-03-30 18:46:30 +1100

    update build.sh to make my own life easier

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker build --rm -t restic-backup .
+VERSION_TAG=0.18.0
+
+docker build -t quay.io/mdusher/restic-backup-docker:latest .
+docker tag quay.io/mdusher/restic-backup-docker:latest quay.io/mdusher/restic-backup-docker:${VERSION_TAG}
