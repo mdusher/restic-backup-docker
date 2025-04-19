34edd3abaa2a8c88df23a5e724fe58a153eabc1c
Michael Usher <github@usher.au>
2025-04-19 20:38:29 +1000

    Add timestamps to entry output

--- files/entry.sh
+++ files/entry.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-echo "Starting container ..."
+#!/bin/sh
+
+log() { echo "[$(date +"%Y-%m-%d %H:%M:%S")] $1"; }
+
+log "Starting container ..."
 
 restic snapshots ${RESTIC_INIT_ARGS} &>/dev/null
 status=$?
-echo "Check Repo status $status"
+log "Check Repo status $status"
 
 if [ $status != 0 ]; then
-    echo "Restic repository '${RESTIC_REPOSITORY}' does not exists. Running restic init."
+    log "Restic repository '${RESTIC_REPOSITORY}' does not exists. Running restic init."
     restic init ${RESTIC_INIT_ARGS}
 
     init_status=$?
-    echo "Repo init status $init_status"
+    log "Repo init status $init_status"
 
     if [ $init_status != 0 ]; then
-        echo "Failed to init the repository: '${RESTIC_REPOSITORY}'"
+        log "Failed to init the repository: '${RESTIC_REPOSITORY}'"
         exit 1
     fi
 fi
 
-echo "Setup backup cron job with cron expression BACKUP_CRON: ${BACKUP_CRON}"
+log "Setup backup cron job with cron expression BACKUP_CRON: ${BACKUP_CRON}"
 echo "${BACKUP_CRON} /usr/bin/flock -n /var/run/backup.lock /bin/backup >> /proc/1/fd/1 2>&1" > /var/spool/cron/crontabs/root
 
 # If CHECK_CRON is set we will enable automatic backup checking
 if [ -n "${CHECK_CRON}" ]; then
-    echo "Setup check cron job with cron expression CHECK_CRON: ${CHECK_CRON}"
+    log "Setup check cron job with cron expression CHECK_CRON: ${CHECK_CRON}"
     echo "${CHECK_CRON} /usr/bin/flock -n /var/run/backup.lock /bin/check >> /proc/1/fd/1 2>&1" >> /var/spool/cron/crontabs/root
 fi
 
@@ -39,6 +43,6 @@ if [ ! -f "${RCLONE_CONF}" ]; then
     touch "${RCLONE_CONF}"
 fi
 
-echo "Container started."
+log "Container started."
 
 exec "$@"
